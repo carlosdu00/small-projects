f61718d1cd882085f97a3ea5ad44aea09ccd658a
carlosdu00 <carloscreed@live.com>
2021-05-04 23:18:22 -0300

    update

--- Numbers Numbers Numbers/script.js
+++
@@ -1,6 +0,0 @@
-function calculate (){
-    let inputnumber = document.getElementById("inputnumber")
-    if(inputnumber.value.length === 0){
-        window.alert("Please, insert a number")
-    }
-}
