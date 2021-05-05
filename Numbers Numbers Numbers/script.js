1299cc9f12b5ff434c666245739c9c9aa10b9cc9
carlosdu00 <carloscreed@live.com>
2021-05-04 23:10:53 -0300

    update

---
+++ Numbers Numbers Numbers/script.js
@@ -0,0 +1,6 @@
+function calculate (){
+    let inputnumber = document.getElementById("inputnumber")
+    if(inputnumber.value.length === 0){
+        window.alert("Please, insert a number")
+    }
+}
