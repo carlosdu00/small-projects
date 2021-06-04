d7ba01ae48b8c65ca6091ad76bbda6d2ace172ca
carlosdu00 <61929315+carlosdu00@users.noreply.github.com>
2021-06-04 17:14:00 -0300

    Add files via upload

---
+++ Moto/script.js
@@ -0,0 +1,11 @@
+function main() {
+
+    const moto = {
+        velocidade : 0,
+        marcha : 0,
+        acelerar : function(){if (this.marcha != 0) {this.velocidade=+5}},
+        freiar : function(){if (this.marcha != 0) {this.velocidade=-5}},
+        subirMarcha : function(){if (this.marcha < 5 && ) {}},
+    }
+
+}
